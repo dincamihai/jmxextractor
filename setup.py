87f9e2fd09a9ae0cefee70b6c452c9d9772e0be8
Mihai Dinca <dincamihai@gmail.com>
2015-10-07 00:16:51 +0300

    update setup.py to run tests

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup
+import sys
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 setup(name='jmxextractor',
       version='0.1',
@@ -8,4 +31,12 @@ setup(name='jmxextractor',
       author_email='dincamihai@gmail.com',
       license='GNU GPL v2.0',
       packages=['jmxextractor'],
+      install_requires=[
+        'lxml==3.4.4'
+      ],
+      tests_require=[
+        'pytest==2.8.1',
+        'pytest-pythonpath==0.7'
+      ],
+      cmdclass = {'test': PyTest},
       zip_safe=False)
